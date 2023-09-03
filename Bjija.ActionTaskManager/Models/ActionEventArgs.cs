c01e38417419a3ac2096da8883328fd398d354ec
mohamed.bouchalkha <mohamed.bouchalkha@exova.com>
2023-09-03 00:14:42 -0400

    Added XML documentation and replaced EventHandler with Action<T>. Fixes #1

--- Bjija.ActionTaskManager/Models/ActionEventArgs.cs
+++ Bjija.ActionTaskManager/Models/ActionEventArgs.cs
@@ -1,18 +1,40 @@
 ﻿namespace Bjija.ActionTaskManager.Models
 {
+    /// <summary>
+    /// Defines an interface for action event arguments that can hold shared data.
+    /// </summary>
     public interface IActionEventArgs
     {
+        /// <summary>
+        /// Gets a dictionary of shared data that can be accessed by multiple tasks.
+        /// </summary>
         Dictionary<string, object> SharedData { get; }
     }
 
+    /// <summary>
+    /// Provides a strongly-typed version of <see cref="EventArgs"/> for action events.
+    /// </summary>
+    /// <typeparam name="TData">The type of the data associated with the action.</typeparam>
     public class ActionEventArgs<TData> : EventArgs, IActionEventArgs
     {
+        /// <summary>
+        /// Gets the data associated with the action event.
+        /// </summary>
         public TData Data { get; }
+
+        /// <summary>
+        /// Gets a dictionary of shared data that can be accessed by multiple tasks.
+        /// </summary>
         public Dictionary<string, object> SharedData { get; } = new();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActionEventArgs{TData}"/> class.
+        /// </summary>
+        /// <param name="data">The data to associate with the action event.</param>
         public ActionEventArgs(TData data)
         {
             Data = data;
         }
     }
+
 }
--- Bjija.ActionTaskManager/Models/PipelineEventArgs.cs
+++ Bjija.ActionTaskManager/Models/PipelineEventArgs.cs
@@ -1,26 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-namespace Bjija.ActionTaskManager.Models
+﻿namespace Bjija.ActionTaskManager.Models
 {
+    /// <summary>
+    /// Provides a strongly-typed version of <see cref="EventArgs"/> for pipeline events.
+    /// </summary>
+    /// <typeparam name="TData">The type of the data associated with the pipeline event.</typeparam>
     public class PipelineEventArgs<TData> : EventArgs
     {
+        /// <summary>
+        /// Gets the data associated with the pipeline event.
+        /// </summary>
         public TData Data { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PipelineEventArgs{TData}"/> class.
+        /// </summary>
+        /// <param name="data">The data to associate with the pipeline event.</param>
         public PipelineEventArgs(TData data)
         {
             Data = data;
         }
     }
 
+    /// <summary>
+    /// Provides a strongly-typed version of <see cref="PipelineEventArgs{TData}"/> for pipeline error events.
+    /// </summary>
+    /// <typeparam name="TData">The type of the data associated with the pipeline event.</typeparam>
     public class PipelineErrorEventArgs<TData> : PipelineEventArgs<TData>
     {
+        /// <summary>
+        /// Gets the exception that caused the pipeline to fail.
+        /// </summary>
         public Exception Error { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PipelineErrorEventArgs{TData}"/> class.
+        /// </summary>
+        /// <param name="data">The data to associate with the pipeline event.</param>
+        /// <param name="error">The exception that caused the pipeline to fail.</param>
         public PipelineErrorEventArgs(TData data, Exception error) : base(data)
         {
             Error = error;
